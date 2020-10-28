80fd28e5d29c692dfde06878d83803c608583a57
MunCL <72634511+MunCL@users.noreply.github.com>
2020-10-28 20:53:46 +0700

    Add files via upload

---
+++ Tabel Perkalian_.cpp
@@ -0,0 +1,28 @@
+/*Program Tabel Perkalian*/
+/*Muniroh 20051397017*/
+
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+
+using namespace std;
+
+main(){
+
+	int i,j;
+	cout<<"===============>>Tabel Perkalian \n";
+	cout<<endl;
+ 
+
+	for (i=1;i<=10;i++){ 	
+	cout<<setw(5)<<"*";
+	
+	for (j=1;j<=10;j++)
+	cout<<setw(5)<<"*";
+	cout<<endl;
+
+}
+
+	getch();
+
+}
